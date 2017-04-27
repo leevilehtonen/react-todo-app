2d0efe1d2be235f3acd6400c96e61041501ec11f
Leevi Lehtonen <leevi.k.lehtonen@helsinki.fi>
2017-04-27 19:30:01 +0300

    firebase update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as firebase from 'firebase';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './css/CustomBootstrap.css';
 import './css/index.css';
+
+
+var config = {
+  apiKey: "AIzaSyAPCNJmJnC166l_jC9lrKAS1sBDg-tsHUI",
+  authDomain: "react-todo-app-183fd.firebaseapp.com",
+  databaseURL: "https://react-todo-app-183fd.firebaseio.com",
+  projectId: "react-todo-app-183fd",
+  storageBucket: "react-todo-app-183fd.appspot.com",
+  messagingSenderId: "142998327441"
+}
+firebase.initializeApp(config);
+
+var database = firebase.database();
 
 
 ReactDOM.render(
